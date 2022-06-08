325736695551299500b8c4bcbe5a0b158732656c
Drixner <drixnerrafael1@gmail.com>
2022-06-07 21:46:27 -0700

    4. Hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Punto de entrada
+ *
+ * Return: siempre 0 (afirmativo)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
