2a75146f3250edc6ed6afdffcb1f9b6c922d3e66
Drixner <drixnerrafael1@gmail.com>
2022-06-07 21:53:47 -0700

    5. Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Punto de entrada
+ *
+ * Return: siempre 0 (afirmativo)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+}
