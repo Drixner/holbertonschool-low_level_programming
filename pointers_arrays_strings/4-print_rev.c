ee11430ad67278071c6fcb1bfe752675f1bbbdb2
Drixner <drixnerrafael1@gmail.com>
2022-06-16 14:53:27 -0700

    Write a function that prints a string, in reverse,

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - Write a function that prints a string, in reverse
+ * @s : varible str rev
+ */
+
+void print_rev(char *s)
+{
+	int x;
+
+	for (x = 8; s[x] != '\0'; x++)
+		;
+
+	for (x--; x >= 0; x--)
+		_putchar(s[x]);
+
+	_putchar(10);
+}
