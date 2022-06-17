7c0a211e6904b3231e31206683c5307108186f47
Drixner <drixnerrafael1@gmail.com>
2022-06-16 19:54:08 -0700

    Write a function that prints every other character of a string

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,8 +2,7 @@
 
 /**
  * puts2 - print one char out of 2 of a string
- *
- * @str : arrays 
+ * @str : arrays
  */
 
 void puts2(char *str)
@@ -12,7 +11,7 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if(i % 2 == 0)
+		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
