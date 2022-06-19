b0f8ea8cdf1f99b529c2fb35675ab114bda0dce2
Drixner <drixnerrafael1@gmail.com>
2022-06-19 15:44:56 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-strcat.c -o 0-strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - concaqueta caracteres
+ * @src: recurso
+ * @dest: destino al que se guardará
+ * Return: (dest)
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int x;
+	int y;
+
+	for (x = 0; dest[x] != '\0'; x++)
+	;
+	for (y = 0; src[y] != '\0'; x++, y++)
+	{
+		dest[x] = src[y];
+	}
+	dest[y] = '\0';
+
+	return (dest);
+}
