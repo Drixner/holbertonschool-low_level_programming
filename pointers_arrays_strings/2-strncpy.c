4e2ad1e01f9f30b53c680a8c1de6dee13ff9461f
Drixner <drixnerrafael1@gmail.com>
2022-06-18 18:55:01 -0700

    Write a function that copies a string.

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - función que permite copiar de un recursos.
+ * @dest; array de destino.
+ * @src: recurso.
+ * @n: indice del cual se copiará
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	x = 0;
+
+	while (x < n && src[x] != '\0')
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	for ( ;x < n; x++)
+	{
+		dest[x] = '\0';
+	}
+	return (dest);
+}
