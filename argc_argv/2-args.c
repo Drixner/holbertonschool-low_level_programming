3990f13c19e5509120a721311d28df610f55f421
root <root@LAPTOP-OOG73IRO.localdomain>
2022-06-22 17:30:46 -0500

    Write a program that prints all arguments it receives.

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - imprime el nombre del archivo
+ * @argc: argument count
+ * @argv: arguments pass to main as a array of strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
