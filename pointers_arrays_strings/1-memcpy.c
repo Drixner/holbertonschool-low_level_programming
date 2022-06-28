b6d6051f4fb5a2eec22289990f804d037d1fd300
root <root@LAPTOP-OOG73IRO.localdomain>
2022-06-28 16:25:38 -0500

    memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -10,9 +10,9 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *n;
+	char *yolo;
 
-	n = dest;
+	yolo = dest;
 	while (n > 0)
 	{
 		*dest = *src;
@@ -21,5 +21,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 		n--;
 	}
 
-	return (n);
+	return (yolo);
 }
