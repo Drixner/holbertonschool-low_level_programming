7b6ddef88ef98b8215814e09c4a2c98da3b30682
root <root@LAPTOP-OOG73IRO.localdomain>
2022-06-28 23:27:25 -0500

    If malloc fails, terminate process with status 98.

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - alojar memoria usando malloc.
+ * @b: unsingned int memory size to allocate
+ * Return: 92
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+
+	return (98);
+}
