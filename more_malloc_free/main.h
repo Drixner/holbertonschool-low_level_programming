641c8b438621d8629dcc309dbdab8d89be4b4852
root <root@LAPTOP-OOG73IRO.localdomain>
2022-06-29 12:47:16 -0500

    main

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,4 +1,5 @@
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -6,3 +7,4 @@ int _strlen(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+#endif
