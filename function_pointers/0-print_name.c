9e7d7a3a8b131b1b504c3220b56d0280f6c8eb6e
root <root@DESKTOP-5TAPVGO.localdomain>
2022-07-03 23:02:23 -0500

    Write a function that prints a name.

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
-#include "function_pointers.h"
-
+#include"stdlib.h"
 /**
- * print_name - Print a name from a function pointer
- * @name: char string
- * @f: function pointer that takes a string argument
- */
-
+ * print_name - prints a name
+ * @name: pointer to char
+ * @f: pointer to function that returns nothing
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
+	if (name && f && *name != '\0')
 	f(name);
 }
