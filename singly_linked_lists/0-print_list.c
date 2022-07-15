f8d183e0c30812f94562b02a3eec8658f4b34fc9
Drixner <drixnerrafael1@gmail.com>
2022-07-15 00:24:20 -0500

    Write a function that prints all the elements of a list_t list.

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * struct list - singly linked list
+ * @h: pointer 
+ */
+
+size_t print_list(const list_t *h)
+{
+    const list_t *current;
+    size_t c;
+
+    c = 0;
+    current = h;
+    while (current != NULL)
+    {
+        printf("[%d] %s\n", current->len, current->str);
+        current = current->next;
+        c++;
+    }
+    return(c);
+}
