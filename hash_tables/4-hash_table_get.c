f8820068c5d601a22232fc81d3b982854821111c
Drixner <drixnerrafael1@gmail.com>
2022-08-21 17:05:30 -0500

    Write a function that retrieves a value associated with a key.

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - given key, get value
+ * @ht: hash table
+ * @key: key
+ * Return: value; or NULL if not found
+ */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int id;
